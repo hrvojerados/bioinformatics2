f22fbd649b5ad86372bec7832858f4b5d2e65c00
hrvojerados <hrvoje.rados8@gmail.com>
2026-01-06 20:55:15 +0100

    worked on training algorithm
    there are numerical issues to resolve

--- src/HMM.cpp
+++ src/HMM.cpp
@@ -66,8 +66,82 @@ public:
   }
   void train(
     int maxIterations,
-    pair<string, string>& data 
-  ){}
+    vector<pair<string, string>>& data 
+  ){
+    int cnt = 0;
+    for (auto [seq1, seq2] : data) {
+      this->print();
+      if (seq1.size() != seq2.size()) {
+        cerr << "given data is surely not aligned properly\n";
+        return;
+      }
+      vector<int> Y;
+      Y.push_back(24);
+      for (int i = 0; i < seq1.size(); i++) {
+        Y.push_back(symsToNum({seq1[i], seq2[i]}));
+        // cout << symsToNum({seq1[i], seq2[i]}) << " ";
+      }
+      Y.push_back(24);
+      // cout << "\n";
+      for (int iteration = 0; iteration < maxIterations; iteration++) {
+        vector<vector<ld>> alpha = 
+          vector<vector<ld>>
+          (Y.size(),vector<ld>
+           (5, -1));
+        vector<vector<ld>> beta =
+          vector<vector<ld>>
+          (Y.size(), vector<ld>
+           (5, -1));
+        vector<vector<ld>> gamma =
+          vector<vector<ld>>
+          (Y.size(), vector<ld>
+           (5, -1));
+        //idk how to format this
+        vector<vector<vector<ld>>> xi =
+          vector<vector<vector<ld>>>
+          (Y.size(), vector<vector<ld>>
+          (5, vector<ld>
+            (5, -1)));
+        this->calcAlpha(alpha, Y);
+        this->calcBeta(beta, Y);
+        this->calcGamma(alpha, beta, gamma, Y);
+        this->calcXi(alpha, beta, xi, Y);
+
+        printAlphaBetaGammaXi(alpha, beta, gamma, xi, Y);
+        for (int i = 0; i < this->initStateP.size(); i++) {
+          this->initStateP[i] = gamma[0][i];
+        }
+
+        for (int i = 0; i < this->transitionP.size(); i++) {
+          for (int j = 0; j < this->transitionP.size(); j++) {
+            ld numerator = 0;
+            for (int t = 0; t < Y.size(); t++) {
+              numerator += xi[t][i][j]; 
+            }
+            ld denominator = 0;
+            for (int t = 0; t < Y.size() - 1; t++) {
+              denominator += gamma[t][i]; 
+            }
+            this->transitionP[i][j] = numerator / denominator;
+          }
+        }
+        for (int i = 0; i < this->emitP.size(); i++) {
+          for (int k = 0; k < this->emitP[i].size(); k++) {
+            ld numerator = 0;
+            for (int t = 0; t < Y.size(); t++) {
+              if (Y[t] == k)
+                numerator += gamma[t][i]; 
+            }
+            ld denominator = 0;
+            for (int t = 0; t < Y.size() - 1; t++) {
+              denominator += gamma[t][i]; 
+            }
+            this->emitP[i][k] = numerator / denominator;
+          }
+        }
+      }
+    }
+  }
 
   pair<string, string> align() {
     return {"jelena", "hrvoje"};
@@ -94,7 +168,12 @@ public:
     }
     cout << "\n";
     cout << "E:\n";
-    vector<string> stateNames = {"Begin", "Match", "InsertX", "InsertY", "End"};
+    vector<string> stateNames = {
+      "Begin",
+      "Match",
+      "InsertX",
+      "InsertY",
+      "End"};
     for (int i = 0; i < emitP.size(); i++) {
       cout << "state: " << stateNames[i] << "\n";
       for (int j = 0; j < 5; j++) {
@@ -109,11 +188,47 @@ public:
     }
   }
 
-  void printAlphaBetaGammaXi() {
+  void printAlphaBetaGammaXi(
+    vector<vector<ld>>& alpha,
+    vector<vector<ld>>& beta,
+    vector<vector<ld>>& gamma,
+    vector<vector<vector<ld>>>& xi,
+    vector<int>& Y) {
     //prints alpha matrix
     //prints beta matrix
     //prints gamma matrix
     //prints xi matrix
+    cout << "alpha:\n";
+    for (int t = 0; t < Y.size(); t++) {
+      for (int i = 0; i < initStateP.size(); i++) {
+        cout << alpha[t][i] << " "; 
+      }
+      cout << "\n";
+    }
+    cout << "beta:\n";
+    for (int t = 0; t < Y.size(); t++) {
+      for (int i = 0; i < initStateP.size(); i++) {
+        cout << beta[t][i] << " "; 
+      }
+      cout << "\n";
+    }
+    cout << "gamma:\n";
+    for (int t = 0; t < Y.size(); t++) {
+      for (int i = 0; i < initStateP.size(); i++) {
+        cout << gamma[t][i] << " "; 
+      }
+      cout << "\n";
+    }
+    cout << "xi:\n";
+    for (int t = 0; t < Y.size(); t++) {
+      cout << "t = " << t << "\n";
+      for (int i = 0; i < initStateP.size(); i++) {
+        for (int j = 0; j < initStateP.size(); j++) {
+          cout << xi[t][i][j] << " "; 
+        }
+        cout << "\n";
+      }
+    }
   }
 
   void readData(
@@ -140,6 +255,36 @@ public:
       }
     }
     return;
+  }
+  void test1() {
+    vector<int> Y = {24, 4, 5, 24};
+    vector<vector<ld>> alpha = vector<vector<ld>>(4, vector<ld>(5, -1));
+    vector<vector<ld>> beta = vector<vector<ld>>(4, vector<ld>(5, -1));
+    vector<vector<ld>> gamma = vector<vector<ld>>(4, vector<ld>(5, -1));
+    //idk how to format this
+    vector<vector<vector<ld>>> xi =
+      vector<vector<vector<ld>>>
+      (4, vector<vector<ld>>
+       (5, vector<ld>
+        (5, -1)));
+  
+    this->calcAlpha(alpha, Y);
+    this->calcBeta(beta, Y);
+    this->calcGamma(alpha, beta, gamma, Y);
+    this->calcXi(alpha, beta, xi, Y);
+    this->print();
+    this->printAlphaBetaGammaXi(alpha,
+                                beta,
+                                gamma,
+                                xi,
+                                Y);
+  }
+  void test2() {
+    string path = "data/Train_set.txt";
+    vector<pair<string, string>> data;
+    readData(path, data);
+    this->train(10, data);
+    this->print();
   }
 private:
   vector<ld> initStateP;
@@ -171,57 +316,105 @@ private:
     return {numMap[num / 5], numMap[num % 5]};
   }
 
-  ld calcAlpha(
+  ld calcAlphaR(
     vector<vector<ld>>& alpha, 
     int t,
     int i,
     vector<int>& Y) {
     int n_state = initStateP.size();
-    ld alp = 0;
     if (alpha[t][i] != -1)
       return alpha[t][i];
     if (t == 0) {
       return alpha[t][i] = initStateP[i] * emitP[i][Y[t]];
     }
+    ld alp = 0;
     for (int j = 0; j < n_state; j++) {
-      alp += calcAlpha(alpha, t - 1, j, Y) * transitionP[j][i];
+      alp += calcAlphaR(alpha, t - 1, j, Y) * transitionP[j][i];
     }
     alp *= emitP[i][Y[t]];
     return alpha[t][i] = alp;
   }
 
-  ld calcBeta(
+  void calcAlpha(
+    vector<vector<ld>>& alpha, 
+    vector<int>& Y) {
+    int T = alpha.size() - 1;
+    for (int i = 0; i < alpha[0].size(); i++) {
+      calcAlphaR(alpha, T, i, Y);
+    }
+  }
+
+  ld calcBetaR(
     vector<vector<ld>>& beta, 
     int t,
     int i,
     vector<int>& Y) {
-    return 0;
+    int n_state = initStateP.size();
+    if (beta[t][i] != -1)
+      return beta[t][i];
+    int T = Y.size() - 1;
+    if (t == T)
+      return beta[t][i] = 1;
+    ld bet = 0;
+    for (int j = 0; j < n_state; j++) {
+      bet += calcBetaR(beta, t + 1, j, Y) * transitionP[i][j] * emitP[j][Y[t + 1]];
+    }
+    return beta[t][i] = bet;
+  }
+
+  void calcBeta(
+    vector<vector<ld>>& beta, 
+    vector<int>& Y) {
+    int T = beta.size() - 1;
+    for (int i = 0; i < beta[0].size(); i++) {
+      calcBetaR(beta, 0, i, Y);
+    }
   }
 
   void calcGamma(
     vector<vector<ld>>& alpha,
     vector<vector<ld>>& beta,
-    vector<vector<ld>>& gamma) {
-    
+    vector<vector<ld>>& gamma,
+    vector<int>& Y) {
+    int n_state = initStateP.size();
+    for (int t = 0; t < Y.size(); t++) {
+      for (int i = 0; i < n_state; i++) {
+        ld den = 0;
+        for (int j = 0; j < n_state; j++) {
+          den += alpha[t][j] * beta[t][j];
+        }
+        gamma[t][i] = (alpha[t][i] * beta[t][i]) / den;
+      }
+    }
   }
 
   void calcXi(
     vector<vector<ld>>& alpha,
     vector<vector<ld>>& beta,
-    vector<vector<ld>>& xi) {
+    vector<vector<vector<ld>>>& xi,
+    vector<int>& Y) {
+    int n_state = initStateP.size();
+    for (int t = 0; t < Y.size() - 1; t++) {
+      for (int i = 0; i < n_state; i++) {
+        for (int j = 0; j < n_state; j++) {
+          // ld den = 0;
+          // for (int k = 0; k < n_state; k++) {
+          //   for (int l = 0; l < n_state; l++) {
+          //     den += alpha[t][k] * transitionP[k][l] * beta[t + 1][l] * emitP[l][Y[t + 1]];
+          //   }
+          // }
+          ld den = 0;
+          for (int k = 0; k < n_state; k++) {
+            den += alpha[t][k] * beta[t][k];
+          }
+          xi[t][i][j] = (alpha[t][i] * transitionP[i][j] * beta[t + 1][j] * emitP[j][Y[t + 1]]) / den;
+        }
+      }
+    }
   }
 };
 
 int main() {
-  string path = "data/Test_set.txt";
   HMM* model = new HMM();
-  model->print();
-  vector<pair<string, string>> data;
-  model->readData(path, data);
-  for (int i = 0; i < data.size(); i++) {
-    cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>\n";
-    cout << data[i].first << "\n";
-    cout << data[i].second << "\n";
-    cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>\n";
-  }
+  model->test1();
 }
